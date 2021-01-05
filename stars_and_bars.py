05ec914d590c1219ad2e7b55bd17f9fca6f48a17
Anthony Thompson <34thompsonas@cua.edu>
2021-01-05 15:26:49 -0500

    update

--- stars_and_bars.py
+++ stars_and_bars.py
@@ -50,33 +50,48 @@ def count_buckets(sb):
     return buckets
 
 
-if __name__ == "__main__":
-    n = 2  # what the non-negative integers must sum to
-    k = 3  # number of non-negative integers
+def make_stars(num_stars):
+    # represent the integer as string of stars with gaps and return the locations of the gaps
+    return [''] + ['*', ''] * n, range(0, 2*n+1, 2)
 
-    stars = [''] + ['*', ''] * n    # represent the integer as string of stars with gaps
 
-    pos_bar_locs = range(0, 2*n+1, 2)  # locations of the gaps along the stars list
-
-    bar_locs = list(combinations_with_replacement(pos_bar_locs, r=k-1))  # set of all possible bar locations based on number
-    #                                                                   # of non-negative integers
-
-    stars_and_bars = place_bars(stars, bar_locs)  # Generate all variations of bar placements in gaps
+def show_stars_and_bars(sb):
+    """
+    Displays the stars and bars variations along with their bucket counts.
+    :param sb:  List of stars and bars variations.  List of lists.
+    :return: List of bucket counts at each variation
+    """
     print('\n')
     print('Number of ways to write a number (n) as the sum of (k) non-negative integers.')
-    print('\n')
+    # print('\n')
     print(f'n: (number of stars *) = {n}')
-    print(f'k: (numbber of bars |) = {k}', '\n')
+    print(f'k: (numbber of bars |) = {k}',)
 
     print(f'number of possible variations: {len(stars_and_bars)}', '\n')
 
     label_sb = 'Stars and Bars'
     label_bc = 'Bucket Counts'
-    print(f'{label_sb:{6*(n+k)}} {label_bc:{10}}')
+    print(f'{label_sb:{6 * (n + k)}} {label_bc:{10}}')
 
-    bucket_count_list = []    # list to hold the individual bucket count variations
-
-    for item in stars_and_bars:
+    bucket_count_list = []  # list to hold the individual bucket count variations
+    for item in sb:
         current_bucket = count_buckets(item).astype('int')
         print(f'{str(item):{6*(n+k)}}', str(current_bucket))
         bucket_count_list.append(current_bucket)
+
+    return bucket_count_list
+
+
+if __name__ == "__main__":
+    n = 2  # what the non-negative integers must sum to
+    k = 3  # number of non-negative integers
+
+    stars, pos_bar_locs  = make_stars(n)    # represent the integer as string of stars with gaps
+    #                                       # and locations of the gaps which will hold bars
+
+    bar_locs = list(combinations_with_replacement(pos_bar_locs, r=k-1))  # set of all possible bar locations based on
+    #                                                                    # number of non-negative integers
+
+    stars_and_bars = place_bars(stars, bar_locs)  # Generate all variations of bar placements in gaps
+
+    show_stars_and_bars(stars_and_bars)  # show Stars and Bars representation, also returns the Bucket Counts in a list
